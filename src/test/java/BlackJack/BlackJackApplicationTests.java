ed4ce06c8a69959e061b1c4451deebd80cca1f76
Paladyn <p.wadon@gazeta.pl>
2021-02-07 16:34:29 +0100

    spring init

---
+++ src/test/java/BlackJack/BlackJackApplicationTests.java
@@ -0,0 +1,13 @@
+package BlackJack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackJackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
